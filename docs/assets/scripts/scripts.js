d7ecfca0f33991f8b16a5da166f60a23156864b4
REP98 <delfinmundo@gmail.com>
2025-02-06 04:16:40 -0400

    testDocs

--- docs/assets/scripts/scripts.js
+++ docs/assets/scripts/scripts.js
@@ -1,3 +1,34 @@
+async function load_data() {
+    try {
+        const response = await fetch("venezuela.json")
+        const data = response.json()
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
+
+const TPL = (o) => `<article>
+                <details>
+                    <summary class="yellow"><h2>Distrito Capital</h2></summary>
+                    <section class="content">
+                        <h3>Municipios</h3>
+                        <details>
+                            <summary class="blue"><h4>Libertador</h4></summary>
+                            <section class="content">
+                                <h5>Parroquias</h5>
+                                <ul>
+                                    <li><strong>Altagracia</strong></li>
+                                </ul>
+                            </section>
+                        </details>
+                        <div class="red divisor"></div>
+                    </section>
+                </details>
+            </article>`
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#year").innerText = "" + new Date().getFullYear()
+    load_data()
     console.log("Web Cargada")
 })
